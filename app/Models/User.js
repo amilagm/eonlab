79a1e81dfc3320cbe6f898a68994ca0beebc3885
amilagm <amilagm@gmail.com>
2015-01-30 16:47:24 +0530

    Added the database config

---
+++ app/Models/User.js
@@ -0,0 +1,10 @@
+/**
+ * Created by agamlath on 1/30/2015.
+ */
+function User(){
+    this.UserName = '';
+    this.PassWord = '';
+}
+
+module.exports = User;
+
--- server.js
+++ server.js
@@ -2,7 +2,14 @@
  * Created by agamlath on 1/27/2015.
  */
 var Hapi = require('hapi');
+var mongoose = require('mongoose');                     // mongoose for mongodb
 var routes = require('./app/routes');
+var database = require('./config/database');
+var Users = require('./app/Models/User');
+
+var usr = new Users();
+
+//mongoose.connect(database.url);     // connect to mongoDB database on modulus.
 // Create a server with a host and port
 var server = new Hapi.Server();
 server.connection({
